f2933f4bbaa14f3871eebb04e3299725791f7808
Xeno's Pc <Xeno's Pc@DESKTOP-BHIA6D4>
2024-04-07 08:20:11 +0000

    Add project files.

---
+++ PaymentAPI/Models/PaymentDetail.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+
+
+        [Key]
+        public int PaymentDetailId { get; set; }
+
+        [Column(TypeName = "navchar(100)")]
+        public string CardOwnerName { get; set; } = "";
+
+        [Column(TypeName = "navchar(16)")]
+        public string CardNumber { get; set;} = "";
+        [Column(TypeName = "navchar(5)")]
+        public string ExpirationDate { get; set; } = "";
+        [Column(TypeName = "navchar(3)")]
+        public string SecurityCode { get; set; } = "";
+    }
+}
